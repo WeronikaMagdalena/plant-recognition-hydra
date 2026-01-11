a1b02446d9d7fc3152ca530a5e98bd7e1c55a482
WeronikaMagdalena <weronika.wooojcik@gmail.com>
2026-01-11 18:31:18 +0100

    Calculate Inforrmation Gain

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,29 @@ def entropy(y):
 
     return -np.sum(probabilities * np.log2(probabilities))
 
-# information_gain(parent, left, right)
-#
+
+def information_gain(parent, left, right):
+    """
+    parent, left (child), right (child) - arrays of class labels
+    """
+    n_parent = len(parent)
+
+    # Invalid split
+    if n_parent == 0 or len(left) == 0 or len(right) == 0:
+        return 0.0
+
+    parent_entropy = entropy(parent)
+
+    weight_left = len(left) / n_parent
+    weight_right = len(right) / n_parent
+
+    children_entropy = (
+            weight_left * entropy(left) +
+            weight_right * entropy(right)
+    )
+
+    return parent_entropy - children_entropy
+
 # split_information(left, right)
 #
 # gain_ratio(parent, left, right)
@@ -22,3 +43,10 @@ def entropy(y):
 # majority_class(y)
 #
 # should_stop(y, depth, max_depth, min_samples)
+
+y_parent = np.array([0, 0, 1, 1, 1])
+y_left   = np.array([0, 0, 1])
+y_right  = np.array([1, 1])
+
+ig = information_gain(y_parent, y_left, y_right)
+print(ig)
